fa7a95e43f526c5b26f5d824438d0d0476aaf338
BernardoALvesdeOliveira <balvesdeoliveira3@gmail.com>
2026-01-17 20:13:59 -0300

    config backend login

--- src/app/components/login-layout/login-layout.ts
+++ src/app/components/login-layout/login-layout.ts
@@ -1,11 +1,59 @@
 import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from '../../services/auth';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login-layout',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, HttpClientModule],
   templateUrl: './login-layout.html',
   styleUrl: './login-layout.css',
 })
 export class LoginLayout {
+
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  emailValido(email: string): boolean {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  }
+
+  enviarFormulario() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return
+    }
+
+    const email = this.loginForm.value.email;
+    const senha = this.loginForm.value.senha;
+
+    if (!this.emailValido(email)) {
+      console.error('Senha inválida');
+      return;
+    }
+
+    // Enviar para API
+    this.authService.login({
+      email: email,
+      password: senha
+    }).subscribe({
+      next: (response) => {
+        console.log('Resposta da API:', response);
+      },
+      error: (err) => {
+        console.error('Erro na requisição', err);
+      }
+    });
+
+  }
 
 }
---
+++ src/app/services/auth.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private apiUrl = 'http://100.68.183.57:8080';
+
+  constructor(private http: HttpClient) {}
+
+  login(data: LoginRequest): Observable<boolean> {
+    return this.http.post<boolean>(
+      `${this.apiUrl}/user/login`,
+      data
+    );
+  }
+}
